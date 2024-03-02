51fb023ea530d47025106dcac9c7eedfa2c8bd16
Noah Nassiri <106232024+Noah316-spec@users.noreply.github.com>
2024-03-02 06:56:12 +0100

    Add files via upload

---
+++ Programm Schichtpläne/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Programm_Schichtpläne
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnDruck = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnDruck
+            // 
+            this.btnDruck.Location = new System.Drawing.Point(320, 41);
+            this.btnDruck.Name = "btnDruck";
+            this.btnDruck.Size = new System.Drawing.Size(124, 39);
+            this.btnDruck.TabIndex = 0;
+            this.btnDruck.Text = "Drucken";
+            this.btnDruck.UseVisualStyleBackColor = true;
+            this.btnDruck.Click += new System.EventHandler(this.btnDruck_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Noah Nassiri";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Frühschicht",
+            "Spätschicht",
+            "Nachtschicht"});
+            this.comboBox1.Location = new System.Drawing.Point(24, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(164, 42);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(136, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Kommi-Tag am Feiertag";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Visible = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(164, 74);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(94, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Feiertagsarbeit";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(492, 134);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDruck);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Druck";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDruck;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Programm Schichtpläne/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Programm_Schichtpläne
+{
+   
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        public static DateTime Now { get; } // Uhrzeit holen
+        public string t = "";
+        public string t1 = "";
+       
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Interval = 1000; // Setzt das Intervall auf 1 Sekunde (1000 Millisekunden)
+            timer.Tick += new EventHandler(timer1_Tick); // Fügt das Event hinzu, das bei jedem Tick aufgerufen wird
+            timer.Start(); // Startet den Time
+            
+        }
+        public static DateTime GetQuarterEnd(DateTime date)
+        {
+            int quarterNumber = (date.Month - 1) / 3+1;
+            return new DateTime(date.Year, quarterNumber * 3, DateTime.DaysInMonth(date.Year, quarterNumber * 3));
+        }
+        public void druck(string pfad)
+        {
+            // Erstellen Sie eine neue Anwendung.
+            Excel.Application excelApp = new Excel.Application();
+
+            // Öffnen Sie die Excel-Datei.
+            Excel.Workbook workbook = excelApp.Workbooks.Open(pfad);
+
+            // Drucken Sie das gesamte Arbeitsbuch aus.
+            workbook.PrintOutEx();
+
+            // Schließen Sie das Arbeitsbuch und die Anwendung.
+            workbook.Close();
+            excelApp.Quit();
+        }
+        public void abfragefrüMO(string pfad)
+        {
+            
+            if ((t == "Di") || (t == "Mi") || (t == "Do") || (t == "Fr"))
+            {
+                pfad = @"L:\\operator\\SCHICHT\\1.    Frühschicht\\2.    Frühschicht Dienstag - Freitag.xlsx";
+                druck(pfad);
+                pfad = "";
+            }
+            else if (t == "Mo")
+            {
+                pfad = @"L:\\operator\\SCHICHT\\1.    Frühschicht\\1.    Frühschicht Montag.xlsx";
+                druck(pfad);
+                pfad = "";
+            }
+            else if (t == "Sa")
+            {
+                if (radioButton1.Checked == true)
+                {
+                    pfad = @"L:\\operator\\SCHICHT\\1.    Frühschicht\\4.    Frühschicht Samstag Kommi.xlsx";
+                    druck(pfad);
+                    pfad = "";
+                }
+                pfad = @"L:\\operator\\SCHICHT\\1.    Frühschicht\\3.    Frühschicht Samstag.xlsx";
+                druck(pfad);
+                pfad = "";
+            }
+
+
+        }
+        private void btnDruck_Click(object sender, EventArgs e)
+        {
+            DateTime date = DateTime.Now; // Setzen Sie hier Ihr Datum
+            DateTime quarterEnd = GetQuarterEnd(date);
+
+            DateTime letzterTagDesMonats = new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));
+
+
+            string pfad = "";
+            if (comboBox1.SelectedIndex == 0)
+            {
+                if (t1 == "01")
+                {
+                    pfad = @"L:\\operator\\SCHICHT\\4.    Sonstige\\1.    Erste Schicht im Monat.xlsx";
+                    druck(pfad);
+                    pfad = "";
+                    abfragefrüMO(pfad);
+                }
+                else
+                {
+                    abfragefrüMO(pfad);
+                }
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                if ((t == "Mo") || (t == "Di") || (t == "Mi") || (t == "Do") || (t == "Fr"))
+                {
+                    pfad = @"L:\\operator\\SCHICHT\\2.    Spätschicht\\Spätschicht Montag - Freitag.xlsx";
+                    druck(pfad);
+                    pfad = "";
+                }
+                else if (t == "Sa")
+                {
+                    pfad = @"L:\\operator\\SCHICHT\\2.    Spätschicht\\Spätschicht Samstag.xlsx";
+                    druck(pfad);
+                    pfad = "";
+                }
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                if ((t == "Mo") || (t == "Di") || (t == "Mi") || (t == "Do"))
+                {
+                    pfad = @"L:\\operator\\SCHICHT\\3.    Nachtschicht\\2.    Nachtschicht Montag - Donnerstag.xlsx";
+                    druck(pfad);
+                    pfad = "";
+                    if (date.Date == quarterEnd)
+                    {
+                        pfad = @"L:\\operator\\SCHICHT\\4.    Sonstige\\3.    Quartalsende.xlsx";
+                        druck(pfad);
+                        pfad = "";
+
+                    }
+                    if (date.Date == letzterTagDesMonats)
+                    {
+                        pfad = @"L:\\operator\\SCHICHT\\4.    Sonstige\\2.    Letzte Schicht im Monat.xlsx";
+                        druck(pfad);
+                        pfad = "";
+                    }
+                }
+                else if (t == "Fr")
+                {
+                    pfad = @"L:\\operator\\SCHICHT\\3.    Nachtschicht\\3.    Nachtschicht Freitag.xlsx";
+                    druck(pfad);
+                    pfad = "";
+                    if (date.Date == quarterEnd)
+                    {
+                        pfad = @"L:\\operator\\SCHICHT\\4.    Sonstige\\3.    Quartalsende.xlsx";
+                        druck(pfad);
+                        pfad = "";
+                    }
+                    if (date.Date == letzterTagDesMonats)
+                    {
+                        pfad = @"L:\\operator\\SCHICHT\\4.    Sonstige\\2.    Letzte Schicht im Monat.xlsx";
+                        druck(pfad);
+                        pfad = "";
+                    }
+                }
+                else if (t == "Mo")
+                {
+                    if (radioButton1.Checked == true) // Nachtschicht Montag Feiertag
+                    {
+                        pfad = @"L:\\operator\\SCHICHT\\3.    Nachtschicht\\4.    Nachtschicht Montag Feiertag Kommi.xlsx";
+                        druck(pfad);
+                        pfad = "";
+                        if (date.Date == quarterEnd)
+                        {
+                            pfad = @"L:\\operator\\SCHICHT\\4.    Sonstige\\3.    Quartalsende.xlsx";
+                            druck(pfad);
+                            pfad = "";
+                        }
+                        if (date.Date == letzterTagDesMonats)
+                        {
+                            pfad = @"L:\\operator\\SCHICHT\\4.    Sonstige\\2.    Letzte Schicht im Monat.xlsx";
+                            druck(pfad);
+                            pfad = "";
+                        }
+                    }
+                }
+            }
+            if (radioButton2.Checked == true)
+            {
+                pfad = @"L:\\operator\\SCHICHT\\4.    Sonstige\\5.    Feiertagsarbeit.xlsx";
+                druck(pfad);
+                pfad = "";
+            }
+
+            
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            t = DateTime.Now.ToString("ddd");
+            t1= DateTime.Now.ToString("dd");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                if (t == "Sa")
+                {
+                    radioButton1.Visible = true;
+                }
+                radioButton1.Visible = false;
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                if (t == "Mo")
+                {
+                    radioButton1.Visible = true;
+                }
+                radioButton1.Visible = false;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+           if(radioButton2.Checked == true)
+            {
+                radioButton1.Checked = false;
+            }
+            else
+            {
+                radioButton2.Checked = true;
+            }
+        }
+
+      
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
